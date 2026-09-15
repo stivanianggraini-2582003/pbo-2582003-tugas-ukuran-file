38b6127985da9639d6da55e2275918780c40a1a4
wputr <2582003@unai.edu>
2026-09-15 09:22:37 +0700

    Menambahkan perhitungan ukuran file

--- src/UkuranFile.java
+++ src/UkuranFile.java
@@ -15,3 +15,36 @@ public class UkuranFile {
         ukuranByte = scanner.nextLong();
     }
 }
+import java.util.Scanner;
+
+public class UkuranFile {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        final double SATU_KB = 1024.0;
+
+        String namaFile;
+        long ukuranByte;
+
+        double ukuranKB;
+        double ukuranMB;
+        double ukuranGB;
+
+        int ukuranMBBulat;
+        double selisihPembulatan;
+
+        System.out.print("Nama file     : ");
+        namaFile = scanner.nextLine();
+
+        System.out.print("Ukuran (byte) : ");
+        ukuranByte = scanner.nextLong();
+
+        ukuranKB = ukuranByte / SATU_KB;
+        ukuranMB = ukuranKB / SATU_KB;
+        ukuranGB = ukuranMB / SATU_KB;
+
+        ukuranMBBulat = (int) ukuranMB;
+        selisihPembulatan = ukuranMB - ukuranMBBulat;
+    }
+}
