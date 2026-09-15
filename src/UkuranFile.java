1e7c9591b320a91a9e49edf927ba7ac30ed6133c
wputr <2582003@unai.edu>
2026-09-15 09:21:36 +0700

    Menambahkan class dan input ukuran file

--- src/UkuranFile.java
+++ src/UkuranFile.java
@@ -5,3 +5,13 @@ public class UkuranFile {
 
         Scanner scanner = new Scanner(System.in);
 
+        String namaFile;
+        long ukuranByte;
+
+        System.out.print("Nama file     : ");
+        namaFile = scanner.nextLine();
+
+        System.out.print("Ukuran (byte) : ");
+        ukuranByte = scanner.nextLong();
+    }
+}
